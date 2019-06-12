d15806a113b9104a371bbcabacfe5713522817b9
Rachel Reinauer <rereinauer@gmail.com>
2019-06-12 17:36:49 -0400

    cart 1 row rendering

---
+++ client/components/cart.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {makeStyles} from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItemText from '@material-ui/core/ListItemText'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import FolderIcon from '@material-ui/icons/Folder'
+import DeleteIcon from '@material-ui/icons/Delete'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2)
+  },
+  avatar: {
+    color: 'white'
+  }
+}))
+const Cart = props => {
+  const classes = useStyles
+  return (
+    <Grid item xs={12} md={6}>
+      <Typography variant="h6" className={classes.title}>
+        Avatar with text and icon
+      </Typography>
+      <div className={classes.demo}>
+        <List>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar className={classes.avatar} src="icecream_icon.png" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Single-line item"
+              secondary="Secondary text"
+            />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="Delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </List>
+      </div>
+    </Grid>
+  )
+}
+
+// mapDispatchToProps = dispatch => ({})
+
+// mapStateToProps = state => ({})
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Cart)
+export default Cart
