196f6c6620c5d8aa37229a5bbd4980b31c05da4b
Rachel Reinauer <rereinauer@gmail.com>
2019-06-14 14:15:27 -0400

    drawer closing at checkout click

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -100,7 +100,11 @@ const Navbar = props => {
         <Cart />
         <div>
           <Link to="/checkout">
-            <Button variant="contained" className={classes.button}>
+            <Button
+              variant="contained"
+              className={classes.button}
+              onClick={toggleDrawer('right', false)}
+            >
               Checkout
             </Button>
           </Link>
